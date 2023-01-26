d6436a67c5c517abe795c8b3763730ed4fca8604
Bruno Divino <brunodivino.dev@gmail.com>
2023-01-26 19:43:35 -0300

    Resultado após vídeo 2.2

---
+++ apps/galeria/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from apps.galeria.models import Fotografia
+
+class FotografiaForms(forms.ModelForm):
+    class Meta:
+        model = Fotografia
+        exclude = ['publicada',]
+
+        widgets = {
+            'nome': forms.TextInput(attrs={'class':'form-control'}),
+            'legenda': forms.TextInput(attrs={'class':'form-control'}),
+            'categoria': forms.Select(attrs={'class':'form-control'}),
+            'descricao': forms.Textarea(attrs={'class':'form-control'}),
+            'foto': forms.FileInput(attrs={'class':'form-control'}),
+            'data_fotografia': forms.DateInput(
+                format = '%d/%m/%Y',
+                attrs={
+                    'type':'date',
+                    'class':'form-control'
+                }
+            ),
+            'usuario': forms.Select(attrs={'class':'form-control'}),
+        }
