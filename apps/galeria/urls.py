29045bfc1adabbb7d2bd8cda968748471912326a
Bruno Divino <brunodivino.dev@gmail.com>
2023-01-26 19:26:12 -0300

    Resultado após vídeo 2.1

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from apps.galeria.views import index, imagem, buscar
+from apps.galeria.views import \
+     index, imagem, buscar, nova_imagem, editar_imagem, deletar_imagem
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
     path('buscar', buscar, name='buscar'),
+    path('nova-imagem', nova_imagem, name='nova_imagem'),
+    path('editar-imagem', editar_imagem, name='editar_imagem'),
+    path('deletar-imagem', deletar_imagem, name='deletar_imagem'),
 ]
--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -29,3 +29,12 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
     return render(request, "galeria/buscar.html", {"cards": fotografias})
+
+def nova_imagem(request):
+    return render(request, 'galeria/nova_imagem.html')
+
+def editar_imagem(request):
+    pass
+
+def deletar_imagem(request):
+    pass
