352176287d0ee93fef6f6075fcc9dfd7a072cf02
Bruno Divino <brunodivino.dev@gmail.com>
2023-02-02 13:19:46 -0300

    Resultado após vídeo 5.1

--- setup/settings.py
+++ setup/settings.py
@@ -118,6 +118,29 @@ USE_I18N = True
 
 USE_TZ = True
 
+# AWS Configuração
+
+AWS_ACCESS_KEY_ID = 'Chave secreta do usuário da aws'
+
+AWS_SECRET_ACCESS_KEY = 'Senha do usuário da aws'
+
+AWS_STORAGE_BUCKET_NAME = 'fotografias-alura-space'
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+AWS_DEFAULT_ACL = 'public-read'
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400'
+}
+
+AWS_LOCATION = 'static'
+
+AWS_QUERYSTRING_AUTH = False
+
+AWS_HEADERS = {
+    'Access-Control-Allow_Origin': '*',
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
