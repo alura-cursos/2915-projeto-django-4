3349e4b331b471dbfcd91e4f7329b22d9e8d7223
Bruno Divino <brunodivino.dev@gmail.com>
2023-02-02 13:55:16 -0300

    Resultado após vídeo 5.3

--- setup/settings.py
+++ setup/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 
 # AWS Configuração
 
-AWS_ACCESS_KEY_ID = 'Chave secreta do usuário da aws'
+AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
 
-AWS_SECRET_ACCESS_KEY = 'Senha do usuário da aws'
+AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
 
-AWS_STORAGE_BUCKET_NAME = 'fotografias-alura-space'
+AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
